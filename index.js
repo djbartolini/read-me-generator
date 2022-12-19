7189b6fc03734b79d96c2f7b03554ff761c776e3
Daniel Bartolini <djb3149@gmail.com>
2022-12-19 18:56:20 -0500

    added inquirer prompt question array

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-inquirer.prompt([
-    {
-        type: 'input',
-        message: 'Title: What is your project\'s title?',
-        name: 'title',
-    },
-    {
-        type: 'input',
-        message: 'Description: Write a brief description about your project:',
-        name: 'description',
-    },
-    {
-        type: 'input',
-        message: 'Installation: How do you run your project?',
-        name: 'installation',
-    },
-    {
-        type: 'list',
-        message: 'License: Which license are you using?',
-        name: 'license',
-        choices: ['Apache', 'MIT', 'Creative Commons', 'Eclipse Public License'],
-    },
-]).then((response) =>
-    fs.appendFile('README.md', `${JSON.stringify(response)}\n`, (err) =>
-        err ? console.log('Error') : console.log('Data saved! Generating README!')));
+const questions = [{
+    type: 'editor',
+    message: 'Title: What is your project\'s title?',
+    name: 'title',
+},
+{
+    type: 'editor',
+    message: 'Description: Write a brief description about your project:',
+    name: 'description',
+},
+{
+    type: 'editor',
+    message: 'Installation: How do you run your project?',
+    name: 'installation',
+},
+{
+    type: 'list',
+    message: 'License: Which license are you using?',
+    name: 'license',
+    choices: ['Apache', 'MIT', 'Creative Commons', 'Eclipse Public License'],
+},
+{
+    type: 'input',
+    message: 'Usage: What are the instructions for usage?',
+    name: 'usage',
+},
+{
+    type: 'input',
+    message: 'Contributing: Who would you like to give credit to?',
+    name: 'contributing',
+}]
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+        
+    
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+        fs.appendFile('info.txt', `${JSON.stringify(response)}\n`, (err) =>
+            err ? console.log('Error') : console.log('Data saved! Generating README!'))}
+    )
+}
+
+// Function call to initialize app
+init();
