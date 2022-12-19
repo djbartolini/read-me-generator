978a48dc92884eb20e79856492c66fcd06d13563
Daniel Bartolini <djb3149@gmail.com>
2022-12-19 17:40:28 -0500

    added inquirer prompts

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
-// inquirer.createPromptModule([
-
-// ])
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'Title: What is your project\'s title?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Description: Write a brief description about your project:',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Installation: How do you run your project?',
+        name: 'installation',
+    },
+    {
+        type: 'list',
+        message: 'License: Which license are you using?',
+        name: 'license',
+        choices: ['Apache', 'MIT', 'Creative Commons', 'Eclipse Public License'],
+    },
+]).then((response) =>
+    fs.appendFile('README.md', `${JSON.stringify(response)}\n`, (err) =>
+        err ? console.log('Error') : console.log('Data saved! Generating README!')));
