94a91e1e38a6e7cdc5b00e6d62b295728414598a
Daniel Bartolini <djb3149@gmail.com>
2022-12-19 19:35:46 -0500

    added switch case for renderlicencelink

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,12 +10,27 @@ function renderLicenseBadge(license) {
       return licenseBadge = '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)]';
     case 'Eclipse Public License':
       return licenseBadge = '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)]';
+    case 'None':
+      return licenseBadge = '';
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'Apache':
+      return licenseLink = '(https://opensource.org/licenses/Apache-2.0)'
+    case 'MIT': 
+      return licenseLink = '(https://opensource.org/licenses/MIT)'
+    case 'Creative Commons':
+      return licenseLink = '(http://creativecommons.org/publicdomain/zero/1.0/)'
+    case 'Eclipse Public License':
+      return licenseLink = '(https://opensource.org/licenses/EPL-1.0)'
+    case 'None':
+      return licenseBadge = '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
